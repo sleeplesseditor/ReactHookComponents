8c1c3b9314236b316268b1459fbb6911c82f84a7
sleeplesseditor <josh@bryson.me.uk>
2020-09-24 23:58:57 +0100

    Menu icon update

--- src/components/Menu/Icons/IconSelector.jsx
+++ src/components/Menu/Icons/IconSelector.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { ReactComponent as ArrowIcon } from './arrow.svg';
+import { ReactComponent as ErrorIcon } from './errorIcon.svg';
 import { ReactComponent as ImageIcon } from './image.svg';
 import { ReactComponent as LoadingIcon } from './loading.svg';
 import { ReactComponent as ShortcutIcon } from './shortcutIcon.svg';
@@ -9,6 +10,7 @@ import { ReactComponent as UserIcon } from './userIcon.svg';
 const getIcon = (icon) => {
   const iconSelection = {
       arrowIcon: () => <ArrowIcon/>,
+      errorIcon: () => <ErrorIcon />,
       imageIcon: () => <ImageIcon/>,
       loadingIcon: () => <LoadingIcon/>,
       shortcutIcon: () => <ShortcutIcon/>,
