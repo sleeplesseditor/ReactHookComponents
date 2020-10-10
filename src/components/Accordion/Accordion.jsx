4fbb4db56de612b299f9fd25c31f41ad6b01295f
sleeplesseditor <josh@bryson.me.uk>
2020-10-10 23:56:24 +0100

    Accordion component setup

---
+++ src/components/Accordion/Accordion.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useRef, useEffect } from 'react';
+import CaretIcon from '../Menu/Icons/caret.png';
+import './Accordion.scss';
+
+const Accordion = ({ children, title }) => {
+  const [active, setActive] = useState(false)
+  const contentRef = useRef(null)
+
+  useEffect(() => {
+    contentRef.current.style.maxHeight = active ? `${contentRef.current.scrollHeight}px` : '0px'
+  }, [contentRef, active])
+
+  const toogleActive = () => {
+    setActive(!active)
+  }
+
+  const titleStyle = {
+    fontWeight: 600,
+    fontSize: '14px',
+  }
+
+  return (
+    <div className="accordion-section">
+      <button className="accordion-title" onClick={toogleActive}>
+        <p style={titleStyle}>{title}</p>
+        <span className={active ? 'accordion-icon rotate': 'accordion-icon'}>
+            <img className="accordion-icon-image" src={CaretIcon} alt="caret-icon" />
+        </span>
+      </button>
+
+      <div
+        ref={contentRef}
+        className="accordion-content"
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Accordion;
