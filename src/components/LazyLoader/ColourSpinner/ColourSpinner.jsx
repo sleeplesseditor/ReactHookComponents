877d22edd7e1318c0f42ff0d1687966952e69a48
sleeplesseditor <josh@bryson.me.uk>
2020-10-22 23:58:19 +0100

    Colour spinner setup

---
+++ src/components/LazyLoader/ColourSpinner/ColourSpinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ColourSpinner.scss';
+
+const ColourSpinner = () => (
+    <div className="color-spinner">
+      <span>Loading</span>
+    </div>
+  );
+  
+  export default ColourSpinner;
