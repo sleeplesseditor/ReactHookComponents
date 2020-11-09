5aec26abb0cd1f3156e81d55524a91c9586eca45
sleeplesseditor <josh@bryson.me.uk>
2020-11-09 16:27:08 +0000

    Further update to AccordionContent JSON management

--- src/components/Accordion/AccordionContent.jsx
+++ src/components/Accordion/AccordionContent.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 
-const getAccordionContent = (content) => {
-    for (const [key, value] of Object.entries(content)) {
-        const tagSelector = {
-            h1: () => <h1 className="accordion-h1">{value}</h1>,
-            paragraph: () => <p className="accordion-paragraph">{value}</p>,
-            default: () => <p>TEST</p>
-        }    
-        return (tagSelector[key] || tagSelector.default)()
-    }
+const getAccordionContent = (key, value) => {
+    const tagSelector = {
+        h1: () => <h1 className="accordion-h1" key={key}>{value}</h1>,
+        img: () => <img className="accordion-content-img" src={value} alt="JSON-image" key={key} />,
+        paragraph: () => <p className="accordion-paragraph" key={key}>{value}</p>,
+        default: () => <p>TEST</p>
+    }    
+    return (tagSelector[key] || tagSelector.default)()
 }
 
 export const AccordionContent = ({ content }) => {
-    for (const item in content) {
-        return (
-            <div className="accordion-content-media">
-                {getAccordionContent(content)}
-            </div>
-        )
-    }
+    const results = Object.entries(content).map(([key, value]) => (
+        <div className="accordion-content-media" key={key}>
+            {getAccordionContent(key, value)}
+        </div>
+    ));
+
+    return results;
 }
