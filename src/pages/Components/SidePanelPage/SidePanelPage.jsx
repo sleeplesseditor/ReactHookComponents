25db56a67434e85a00cbc09a0f12d6111749d5ad
sleeplesseditor <josh@bryson.me.uk>
2020-11-12 13:55:26 +0000

    Styling fix for right-side side panel

--- src/pages/Components/SidePanelPage/SidePanelPage.jsx
+++ src/pages/Components/SidePanelPage/SidePanelPage.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react';
 import '../../page-structure.scss';
+import './SidePanelPage.scss';
 import { useOnClickOutside } from '../../../components/Hooks/Helpers/useOnClickOutside';
 import SidePanel from '../../../components/SidePanel/SidePanel';
 
@@ -8,18 +9,34 @@ import {AccordionContent} from '../../../components/Accordion/AccordionContent';
 import Text from '../../../components/Accordion/Test.json';
 
 const SidePanelPage = () => {
-    const [open, setOpen] = useState(false);
+    const [leftOpen, setLeftOpen] = useState(false);
+    const [rightOpen, setRightOpen] = useState(false);
     const node = useRef();
-    useOnClickOutside(node, () => setOpen(false));
+    const closeAll = () => {
+        setLeftOpen(false);
+        setRightOpen(false);
+    }
+    useOnClickOutside(node, () => closeAll());
 
     return (
         <>
             <div className="page-container">
                 <h2 className="page-continer-heading">Side Panel</h2>
+                <p>Click away from the side panels to close them</p>
+                <div className="page-button-container">
+                    <button className="panel-btn" onClick={() => setLeftOpen(!leftOpen)} disabled={leftOpen}>Open Left Panel</button>
+                    <button className="panel-btn" onClick={() => setRightOpen(!rightOpen)} disabled={rightOpen}>Open Right Panel</button>
+                </div>
             </div>
             <div className="page-panel-container" ref={node}>
-                <button className="panel-btn" onClick={() => setOpen(!open)}>Open</button>
-                <SidePanel open={open} setOpen={setOpen} orientation="left">
+                <SidePanel open={leftOpen} setOpen={setLeftOpen} orientation="left">
+                    {Text.map(item => (
+                        <Accordion key={item.sectionTitle} title={item.sectionTitle}>
+                            <AccordionContent content={item} />
+                        </Accordion>
+                    ))}
+                </SidePanel>
+                <SidePanel open={rightOpen} setOpen={setRightOpen} orientation="right">
                     {Text.map(item => (
                         <Accordion key={item.sectionTitle} title={item.sectionTitle}>
                             <AccordionContent content={item} />
