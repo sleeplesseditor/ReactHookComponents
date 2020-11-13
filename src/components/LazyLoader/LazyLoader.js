f58059b689f2cb8c1850cdad21ae4a4d260bc73c
sleeplesseditor <josh@bryson.me.uk>
2020-11-13 18:21:36 +0000

    Minor styling alterations

--- src/components/LazyLoader/LazyLoader.js
+++ src/components/LazyLoader/LazyLoader.js
@@ -1,6 +1,6 @@
 import React, { PureComponent, Suspense } from 'react';
 import ColourSpinner from './ColourSpinner/ColourSpinner';
-import Spinner from './Spinner/Spinner';
+// import Spinner from './Spinner/Spinner';
 
 function LazyLoader(WrappedComponent) {
   return class Wrapped extends PureComponent {
