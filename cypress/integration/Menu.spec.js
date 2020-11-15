b2b6b6eafafe7058e7ef83f2e8d47a7b566ca3a3
sleeplesseditor <josh@bryson.me.uk>
2020-11-15 13:25:59 +0000

    Initial Cypress E2E setup

---
+++ cypress/integration/Menu.spec.js
@@ -0,0 +1,43 @@
+describe('Menu', function() {
+    beforeEach(function() {
+        cy.visit('/')
+    })
+
+    it('should open the menu when the Navbar button is clicked', function() {
+        cy.get('.nav-item').click()
+        cy.get('.dropdown').should('have.length', 1);
+    });
+
+    it('should close the menu when the Navbar button is clicked a second time', function() {
+        cy.get('.nav-item').click()
+        cy.get('.dropdown').should('have.length', 1);
+        cy.get('.nav-item').click()
+        cy.get('.dropdown').should('have.length', 0);
+    });
+
+    it('should close the menu when clicking outside the menu', function() {
+        cy.get('.nav-item').click()
+        cy.get('.dropdown').should('have.length', 1);
+        cy.get('.navbar-heading').click()
+        cy.get('.dropdown').should('have.length', 0);
+    });
+
+    it('should render an SVG icon next to each menu option', function() {
+        cy.get('.nav-item').click()
+        cy.get('.icon-button').should('have.length.greaterThan', 3)
+    })
+
+    it('should render a back option for each sub-menu and return to the first menu when clicked', function() {
+        cy.get('.nav-item').click()
+        cy.get('.menu>.menu-item').eq(1).click();
+        cy.get('.menu-secondary-enter-done>.menu-item').eq(0).click();
+        cy.get('.menu>.menu-item').eq(0).contains('Components');
+    });
+
+    it('should open the menu when clicked, and navigate to a new page', function() {
+        cy.get('.nav-item').click()
+        cy.get('.menu>.menu-item').eq(1).click();
+        cy.get('.menu-secondary-enter-done>.menu-item').eq(1).click();
+        cy.get('.page-continer-heading').contains('useImageLoader Hook')
+    })
+})
