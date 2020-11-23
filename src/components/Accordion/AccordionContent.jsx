756da90f88907b596d5bb3c36995c4a71d9837f0
sleeplesseditor <josh@bryson.me.uk>
2020-11-23 00:02:35 +0000

    Initial Table file setup

--- src/components/Accordion/AccordionContent.jsx
+++ src/components/Accordion/AccordionContent.jsx
@@ -5,16 +5,16 @@ const getAccordionContent = (key, value) => {
     const tagSelector = {
         imgUpper: () => <img className="accordion-content-img" src={value} alt="JSON-image" key={key} />,
         imgLower: () => <img className="accordion-content-img" src={value} alt="JSON-image" key={key} />,
-        paragraph: () => <p className="accordion-paragraph" key={key} dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(value)}}></p>,
-        paragraphUpper: () => <p className="accordion-paragraph" key={key} dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(value)}}></p>,
-        paragraphLower: () => <p className="accordion-paragraph" key={key} dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(value)}}></p>,
+        content: () => <div className="accordion-paragraph-content" key={key} dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(value)}}></div>,
+        table: () => console.log('TABLE', value),
         default: () => null
     }    
+
     return (tagSelector[key] || tagSelector.default)()
 }
 
 export const AccordionContent = ({ content }) => {
-    const results = Object.entries(content).map(([key, value]) => (
+    const results = Object.entries(content).map(([key, value])=> (
         <div className="accordion-content-media" key={key}>
             {getAccordionContent(key, value)}
         </div>
